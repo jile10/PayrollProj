058bfb4be7754f454dec394da3026958a39af381
Unknown <jimbo.jile10@gmail.com>
2018-01-24 18:16:49 +0800

    relationship and addedd tables etc

--- app/Employee.php
+++ app/Employee.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Employee extends Model
 {
-    //
+    public function companies(){
+    	return $this->belongsTo('App\Company');
+    }
+
+    public function role(){
+    	return $this->belongsTo('App\Role');
+    }
 }
